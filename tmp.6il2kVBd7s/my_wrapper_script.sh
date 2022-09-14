0829227da2e88af0c9f1d9325af140560c01f5de
Jorge Avila A <jorge.avila@oracle.com>
2022-09-14 14:47:49 -0300

    Uses custom log names

--- tmp.6il2kVBd7s/my_wrapper_script.sh
+++ tmp.6il2kVBd7s/my_wrapper_script.sh
@@ -4,7 +4,7 @@
 nc -lk -p 80 -e /tmp/nc_reply.sh &
   
 # Start the second process
-java HelloWorld > /var/log/out 2> /var/log/err &
+java HelloWorld > /var/log/${HOSTNAME}_out 2> /var/log/${HOSTNAME}_err &
 
 # Starts crond
 crond -l 2 -f -L /var/log/crond
