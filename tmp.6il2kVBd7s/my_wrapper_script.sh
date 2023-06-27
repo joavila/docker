4560be353fdbcacd89723cddf45f85f33d6a27c5
joavila <jorge.avila@gmail.com>
2023-06-27 10:28:08 -0400

    Redirects output and updates port

--- tmp.6il2kVBd7s/my_wrapper_script.sh
+++ tmp.6il2kVBd7s/my_wrapper_script.sh
@@ -1,10 +1,11 @@
 #!/bin/bash --
 
 # Start the first process
-nc -lk -p 80 -e /tmp/nc_reply.sh &
+nc -lk -p 8888 -e /tmp/nc_reply.sh &
   
 # Start the second process
-java HelloWorld > /var/log/${HOSTNAME}_out 2> /var/log/${HOSTNAME}_err &
+# Redirects output. This allows to see these messages through 'docker log' utility.
+java HelloWorld | tee /var/log/${HOSTNAME}_out 2> /proc/1/fd/2 1> /proc/1/fd/1 &
 
 # Starts crond in the foreground to enable logrotate
 crond -n -m off
