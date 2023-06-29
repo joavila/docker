fc4f4c6de51ac58f1bb70368b217b44d2330abe0
joavila <jorge.avila@gmail.com>
2023-06-28 20:05:56 -0400

    Uses named pipe for communication

--- tmp.6il2kVBd7s/my_wrapper_script.sh
+++ tmp.6il2kVBd7s/my_wrapper_script.sh
@@ -7,5 +7,17 @@ nc -lk -p 8888 -e /tmp/nc_reply.sh &
 # Redirects output. This allows to see these messages through 'docker log' utility.
 java HelloWorld | tee /var/log/${HOSTNAME}_out 2> /proc/1/fd/2 1> /proc/1/fd/1 &
 
-# Starts crond in the foreground to enable logrotate
-crond -n -m off
+# Starts crond to enable logrotate without reporting
+crond -m off
+
+#Creates pipe
+mkfifo /tmp/message
+
+# Reads from pipe until logrotate kicks in
+while read filepath < /tmp/message
+do
+    curl --silent --show-error --fail --upload-file "${filepath}" "${BUCKET}" && \
+	    rm "${filepath}" && \
+	    /bin/echo "Upload OK ${filepath}" > /proc/1/fd/1 || \
+	    /bin/echo "Upload NOK ${filepath}" > /proc/1/fd/2; 
+done
